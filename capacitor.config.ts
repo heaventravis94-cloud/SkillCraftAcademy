652fb954b572af3d9b73547f7e3814bc0532c3ed
Heaven Travis <appmanagement.ht@gmail.com>
2025-12-25 17:29:54 -0500

    Finalize Capacitor Android + Codemagic build setup

---
+++ capacitor.config.ts
@@ -0,0 +1,8 @@
+const config: CapacitorConfig = {
+  appId: 'com.skillcraftacademy',
+  appName: 'SkillCraftAcademy',
+  webDir: 'dist',
+  bundledWebRuntime: false,
+};
+
+export default config;
--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,16 @@
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Layout from "./components/Layout"; // Import the new Layout component
-import Index from "./pages/Index";
-import Library from "./pages/Library";
-import ActivityDetail from "./pages/ActivityDetail";
-import Favorites from "./pages/Favorites";
-import ProgressOverview from "./pages/ProgressOverview";
-import NotFound from "./pages/NotFound";
+import {
+  Toast,
+  ToastClose,
+  ToastDescription,
+  ToastProvider,
+  ToastTitle,
+  ToastViewport,
+} from "@/components/ui/toast";
 
-const queryClient = new QueryClient();
-
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Layout> {/* Wrap routes with the Layout component */}
-          <Routes>
-            <Route path="/" element={<Index />} />
-            <Route path="/library" element={<Library />} />
-            <Route path="/activity/:id" element={<ActivityDetail />} />
-            <Route path="/favorites" element={<Favorites />} />
-            <Route path="/progress" element={<ProgressOverview />} />
-            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </Layout>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
-);
-
-export default App;
+export function Toaster() {
+  return (
+    <ToastProvider>
+      <ToastViewport />
+    </ToastProvider>
+  );
+}
